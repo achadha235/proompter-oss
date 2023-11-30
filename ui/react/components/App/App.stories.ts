c3c4ec20940383b844f2bb68c9c8e80f1491d369
Abhishek Chadha <abhishekchadha235@gmail.com>
2023-11-30 18:08:41 +0530

    chore: cleanup

--- ui/react/components/App/App.stories.ts
+++ ui/react/components/App/App.stories.ts
@@ -2,23 +2,18 @@ import type { Meta, StoryObj } from "@storybook/react";
 
 import { App } from ".";
 
-// More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
 const meta = {
   title: "Components/App",
   component: App,
   parameters: {
-    // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
-    layout: "centered",
+    layout: "fullscreen",
   },
-  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
   tags: ["autodocs"],
-  // More on argTypes: https://storybook.js.org/docs/api/argtypes
 } satisfies Meta<typeof App>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-// More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
 export const Primary: Story = {
   args: { hello: "world" },
 };
--- ui/react/components/App/index.tsx
+++ ui/react/components/App/index.tsx
@@ -5,10 +5,10 @@ export interface AppProps {
   hello?: string;
 }
 
-export function App({ hello = "Yo" }: AppProps) {
+export function App({ hello = "Yo" }: AppProps): React.JSX.Element {
   return (
     <div className="ai-flex ai-justify-center ai-items-center ai-w-screen ai-h-screen ai-bg-red-200">
-      Yo
+      Yo there coolio
     </div>
   );
 }
